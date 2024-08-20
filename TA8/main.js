73b5b1a30fdf96971942dfc5f6344f892eb5ca89
MHC2005 <141636499+MHC2005@users.noreply.github.com>
2024-08-20 07:50:06 -0300

    Add files via upload

---
+++ TA8/main.js
@@ -0,0 +1,29 @@
+function findTheOldest(people){
+    const oldest = people.sort((a,b) => {
+        const lastGuy = a.yearOfDeath - a.yearOfBirth;
+        const nextGuy = b.yearOfDeath - b.yearOfBirth;
+        return lastGuy > nextGuy ? -1 : 1;
+    })
+    return oldest[0];
+
+}
+
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
+
+  console.log(findTheOldest(people));
